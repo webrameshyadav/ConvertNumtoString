d7aa4b811709f0c2a1d3f20525ebb260cc19e8cd
webrameshyadav <83348002+webrameshyadav@users.noreply.github.com>
2022-07-18 18:17:42 +0530

    angular upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -80,7 +80,7 @@ import { TextBoxcustomDirective } from './my-material/text-boxcustom.directive';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AgGridModule.withComponents([]),
+    AgGridModule,
     StoreModule.forRoot({conversionStore: conversionReducer.reducer}),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
@@ -126,6 +126,7 @@ import { TextBoxcustomDirective } from './my-material/text-boxcustom.directive';
     MatTreeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
